2f13236171372a393b0bf3a549ed1d991edce2ff
webdx <2199869794@qq.com>
2020-11-08 17:32:09 +0800

    自定义其他谱,one day

---
+++ pages/alltools/top_bar.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="top_bar">
+		<view class="top-l">
+			<view class="">
+				推荐
+			</view>
+			<view class="uipt">
+				
+				
+			</view>
+			<view class="image_">
+				<image src="../../static/icon/paihangbang.png" mode="concv"></image>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	
+	export default {
+		mounted:function(){
+			console.log("肯定是看看")
+		}
+	}
+</script>
+
+<style land='sass'>
+	.top_bar{
+		width: 100%;
+		border: 1rpx red solid;
+	}
+	.top-l{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		border: 1px solid #007AFF;
+	}
+	.image_{
+		width: 100px;
+		image{
+		width: 100rpx;
+		height: 100rpx;
+		border: #1AAD19 solid 1rpx;
+		}
+	}
+	.image_ image{
+		width: 100rpx;
+		height: 100rpx;
+		border: #1AAD19 solid 1rpx;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="container">
+		<mytop/>
+	</view>
+</template>
+
+
+<script>
+	import mytop from '@/pages/alltools/top_bar.vue'
+	export default {
+		components:{
+			mytop,
+		},
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		},
+		onPageScroll:{
+			
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
